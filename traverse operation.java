b2677c3c5923bda32aedeb9ca3a8a9818cfb43ad
Aakash Bala Prajapathi S <148979879+dragonwarrior-2219@users.noreply.github.com>
2026-08-17 14:52:42 +0530

    Add linked list traversal functionality
    Implement a linked list traversal method to print node values.

---
+++ traverse operation.java
@@ -0,0 +1,35 @@
+class Node {
+    int data;
+    Node next;
+
+    Node(int x) {
+        data = x;
+        next = null;
+    }
+}
+
+public class Main {
+
+    static void traverse(Node head) {
+
+        Node curr = head;
+
+        while (curr != null) {
+            System.out.print(curr.data + " ");
+            curr = curr.next;
+        }
+
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+
+        Node head = new Node(10);
+        head.next = new Node(20);
+        head.next.next = new Node(30);
+        head.next.next.next = new Node(40);
+
+        System.out.println("Linked List Elements:");
+        traverse(head);
+    }
+}
